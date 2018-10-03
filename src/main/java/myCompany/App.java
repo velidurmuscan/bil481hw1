62e609ae9b00742c914e52ed47510a78fca7f72c
Veli Durmuscan <durmuscanveli@gmail.com>
2018-10-03 13:57:12 +0300

    Code files are configured according to the homework requirements. I hope it will work

--- src/main/java/myCompany/App.java
+++ src/main/java/myCompany/App.java
@@ -23,6 +23,21 @@ public class App {
         }
         return false;
     }
+    
+    public static boolean doubleSearch(ArrayList<Integer> arr1, int num1, ArrayList<Integer> arr2, int num2) {
+    	if(arr1==null || arr2==null)
+    		return false;
+    	for(int i:arr1) {
+    		if(i==num1) {
+    			for(int y:arr2) {
+    				if(y==num2) {
+    					return true;
+    				}
+    			}
+    		}
+    	}
+    	return false;
+    }
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
@@ -30,37 +45,38 @@ public class App {
         get("/", (req, res) -> "Hello, World");
 
         post("/compute", (req, res) -> {
-            // System.out.println(req.queryParams("input1"));
-            // System.out.println(req.queryParams("input2"));
 
-            String input1 = req.queryParams("input1");
-			
-			
-			
-			// The code to be implemented
-			
-            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            String array1_ = req.queryParams("array1");
+            String num1_ = req.queryParams("num1").replaceAll("\\s", "");
+            String array2_ = req.queryParams("array2");
+            String num2_ = req.queryParams("num2").replaceAll("\\s", "");
+
+            java.util.Scanner sc1 = new java.util.Scanner(array1_);
             sc1.useDelimiter("[;\r\n]+");
-            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            java.util.ArrayList<Integer> input1List = new java.util.ArrayList<>();
             while (sc1.hasNext()) {
                 int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
-                inputList.add(value);
+                input1List.add(value);
             }
-            System.out.println(inputList);
+            
+            int num1AsInt = Integer.parseInt(num1_);
+            
+            java.util.Scanner sc2 = new java.util.Scanner(array2_);
+            sc2.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> input2List = new java.util.ArrayList<>();
+            while (sc2.hasNext()) {
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s", ""));
+                input2List.add(value);
+            }
+            
+            int num2AsInt = Integer.parseInt(num2_);
 
-            String input2 = req.queryParams("input2").replaceAll("\\s", "");
-            int input2AsInt = Integer.parseInt(input2);
-
-            boolean result = App.search(inputList, input2AsInt);
+            boolean result = App.doubleSearch(input1List, num1AsInt, input2List, num2AsInt);
 
             Map map = new HashMap();
             map.put("result", result);
             return new ModelAndView(map, "compute.mustache");
-			
-			
-			
-			
-			
+		
         }, new MustacheTemplateEngine());
 
         get("/compute", (rq, rs) -> {
--- src/test/java/myCompany/AppTest.java
+++ src/test/java/myCompany/AppTest.java
@@ -41,21 +41,41 @@ public class AppTest
 	
 	
 	public void testFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertTrue(new App().search(array, 4));
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+        assertTrue(new App().doubleSearch(array1, 4, array2, 8));
     }
 
-    public void testNotFound() {
-        ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        assertFalse(new App().search(array, 5));
+    public void testNotFound1() {
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+    	assertFalse(new App().doubleSearch(array1, 0, array2, 8));
+    }
+    
+    public void testNotFound2() {
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+    	assertFalse(new App().doubleSearch(array1, 4, array2, 9));
     }
 
-    public void testEmptyArray() {
-        ArrayList<Integer> array = new ArrayList<>();
-        assertFalse(new App().search(array, 1));
+    public void testEmptyArray1() {
+        ArrayList<Integer> array1 = new ArrayList<>();
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+    	assertFalse(new App().doubleSearch(array1, 4, array2, 8));
+    }
+    public void testEmptyArray2() {
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        ArrayList<Integer> array2 = new ArrayList<>();
+    	assertFalse(new App().doubleSearch(array1, 4, array2, 8));
     }
 
-    public void testNull() {
-        assertFalse(new App().search(null, 1));
+    public void testNull1() {
+        ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+    	assertFalse(new App().doubleSearch(null, 4, array2, 8));
+    }
+    
+    public void testNull2() {
+        ArrayList<Integer> array1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+    	assertFalse(new App().doubleSearch(array1, 4, null, 8));
     }
 }
